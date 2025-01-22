e143b983996190b9dca1d5635de71a6a73fa3826
solutegrate <support@solutegrate.com>
2025-01-22 09:07:26 -0600

    Refactor Webpack build process to conditionally set output folder and streamline dependencies

--- bundle.js
+++ bundle.js
@@ -2,49 +2,38 @@ const path = require('path');
 const fs = require('fs/promises');
 const webpack = require('webpack');
 
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const TerserPlugin = require("terser-webpack-plugin");
 
 const id = 'geoflo-sdk';
-const folder = 'dist';
 const input = 'index.js';
 const args = process.argv;
 const prod = args[2];
+const folder = prod ? 'dist' : 'dev';
 const mode = prod ? 'production' : 'development';
 const name = `${id}.min.js`;
 
 const entry = path.resolve(__dirname, input);
 const output = path.resolve(__dirname, folder);
 
-bundle();
-
-async function bundle() {
-	//const plugins = [ new CleanWebpackPlugin({ cleanOnceBeforeBuildPatterns: [ path.join(output, `${id}.*.js`) ] }) ];
-
-	const options = {
-		mode: mode,
-		watch: false,
-		stats: { colors: true },
-		entry: entry,
-		output: {
-			path: output,
-			filename: name,
-			publicPath: '/'
-		},
-		resolve: { extensions: ['.json', '.js', '.jsx'] },
-		plugins: plugins
-	};
-
-	if (prod) options.optimization = {
+webpack({
+	mode: mode,
+	watch: false,
+	stats: { colors: true },
+	entry: entry,
+	output: {
+		path: output,
+		filename: name,
+		publicPath: '/'
+	},
+	resolve: { extensions: ['.json', '.js', '.jsx'] },
+	optimization: prod ? {
 		minimize: true,
 		minimizer: [
 			new TerserPlugin({
 				terserOptions: {
 					ecma: undefined,
 					parse: {},
-					compress: {
-						drop_console: true
-					},
+					compress: { drop_console: true },
 					mangle: true,
 					output: null,
 					format: null,
@@ -55,13 +44,9 @@ async function bundle() {
 				}
 			})
 		]
-	}
-
-	webpack(options, async (err, stats) => await build(err, stats));
-}
-
-async function build(err, stats) {
-    if (err) return console.error('Error building:', err);
+	} : {}
+}, async function (err, stats) {
+	if (err) return console.error('Error building:', err);
 
 	const data = await fs.readFile(path.join(output, name), 'utf8');
 	if (!data) return console.error('Error handling JS file');
@@ -72,4 +57,4 @@ async function build(err, stats) {
 	} catch (error) {
 		console.error('Error handling CSS file:', error);
 	}
-}
+});
