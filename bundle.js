e847e632ec9141b45f4c10be35bb43fa35fd38e6
solutegrate <support@solutegrate.com>
2025-01-22 08:59:08 -0600

    Update output file naming and improve CSS file handling in bundler

--- bundle.js
+++ bundle.js
@@ -11,7 +11,7 @@ const input = 'index.js';
 const args = process.argv;
 const prod = args[2];
 const mode = prod ? 'production' : 'development';
-const name = `${id}.[contenthash].js`;
+const name = `${id}.min.js`;
 
 const entry = path.resolve(__dirname, input);
 const output = path.resolve(__dirname, folder);
@@ -19,7 +19,7 @@ const output = path.resolve(__dirname, folder);
 bundle();
 
 async function bundle() {
-	const plugins = [ new CleanWebpackPlugin({ cleanOnceBeforeBuildPatterns: [ path.join(output, `${id}.*.js`) ] }) ];
+	//const plugins = [ new CleanWebpackPlugin({ cleanOnceBeforeBuildPatterns: [ path.join(output, `${id}.*.js`) ] }) ];
 
 	const options = {
 		mode: mode,
@@ -63,27 +63,12 @@ async function bundle() {
 async function build(err, stats) {
     if (err) return console.error('Error building:', err);
 
-	console.log(stats.toString({ colors: true }));
-	console.log('Assets by Chunk Name:', stats.toJson().assetsByChunkName);
-	
-	const outputFiles = stats.toJson().assetsByChunkName;
-	const hashedJsFile = Object.values(outputFiles).flat().find(file => file.endsWith('.js'));
-
-    if (!hashedJsFile) {
-        console.error('Error: Unable to find the hashed JS file.');
-        console.log('Emitted assets:', stats.toJson().assets);
-        throw new Error('Error handling output files');
-    }
-
-	console.log('Hashed JS File:', hashedJsFile);
-
-    const bundlePath = path.join(output, hashedJsFile);
-	const data = await fs.readFile(bundlePath, 'utf8');
+	const data = await fs.readFile(path.join(output, name), 'utf8');
 	if (!data) return console.error('Error handling JS file');
 
 	try {
 		const css = await fs.readFile(path.resolve(__dirname, './index.css'), 'utf8');		
-		await fs.writeFile(path.resolve(__dirname, folder + '/' + 'index.css'), css);
+		await fs.writeFile(path.resolve(__dirname, folder + '/' + id + '.css'), css);
 	} catch (error) {
 		console.error('Error handling CSS file:', error);
 	}
