ed38ec95ebe178938a44c0778c1fe5dce5979ea7
solutegrate <support@solutegrate.com>
2025-03-14 13:53:19 -0500

    Fix layer metadata assignment in Layers.js to ensure correct layer object reference and event firing

--- src/Layers.js
+++ src/Layers.js
@@ -491,12 +491,14 @@ const Layers = function () {
         if (!layer || !layer.id) return false;
         layer.metadata = layer.metadata || options;
         if (map.getLayer(layer.id)) {
-            if (!this.layersMap.has(layer.id)) this.layersMap.set(layer.id, map.getLayer(layer.id));
+            layer.layer = map.getLayer(layer.id);
+            if (!this.layersMap.has(layer.id)) this.layersMap.set(layer.id, layer);
             return this.getLayer(layer.id);
         }
         map.addLayer(layer);
-        this.layersMap.set(layer.id, map.getLayer(layer.id));
-        geoflo.fire('layer.add', { id: layer.id, layer: map.getLayer(layer.id) });
+        layer.layer = map.getLayer(layer.id);
+        this.layersMap.set(layer.id, layer);
+        geoflo.fire('layer.add', { id: layer.id, layer: layer });
         return layer;
     };
 
